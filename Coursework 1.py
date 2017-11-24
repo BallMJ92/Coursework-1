e5a0211bcd2feb7e5e7a5a4877248c550f6bddfd
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2017-11-24 20:56:53 +0000

    Coursework 1 Finalised

--- Coursework 1.py
+++ Coursework 1.py
@@ -1,5 +1,6 @@
 class courseworkOne:
 
+    #1.1 List containing the given set of elements
     def checkVals(self):
         print("Exercise 1 - List Processing")
         print("1.1 List containing the given set of elements\n")
@@ -17,20 +18,23 @@ class courseworkOne:
             listLength -= 1
 
         #checking if values appear in list A
-        if (val1 and val2 and val3) in A:
+        if val1 in A and val2 in A and val3 in A:
             print("\nYes")
             print("List containing the given set of elements\n")
         else:
             print("\nNo")
             print("List not containing the given set of elements\n")
 
+    #1.2 list containing the given set of elements in the given order
     def checkValsConsecutiveOrder(self):
+        print("-----------------")
         print("1.2 List containing the given set of elements in the given order\n")
 
         # list
         A = []
         # values to test on list elements
         val1, val2, val3 = 1, 2, 3
+        inOrder = 0
 
         #adding input to variable listLength
         listLength = int(input("Enter number of list elements: "))
@@ -39,18 +43,22 @@ class courseworkOne:
             A.append(addElement)
             listLength -= 1
 
-        #checking if values appear as individual elements in list A
-        output = ""
+
+        output = "\nNo \nList not containing the given set of elements in the given order\n"
+        #checking each item in list and checking the order using the inOrder variable
         for i in A:
-            x = i
-            if (str(val1 and val2 and val3)) in str(x):
-                output = "\nYes \nList containing the given set of elements in the given order"
-            else:
-                output = "\nNo \nList not containing the given set of elements in the given order"
+            if val1 == int(i):
+                inOrder = 1
+            if val2 == int(i) and inOrder == 1:
+                    inOrder = 2
+            if val3 == int(i) and inOrder == 2:
+                    output = "\nYes \nList containing the given set of elements in the given order\n"
 
         print(output)
 
+    #1.3 Lists containing the given set of elements consecutively and in the given order
     def checkValsOrder(self):
+        print("-----------------")
         print("1.3 List containing the given set of elements consecutively and in the given order\n")
 
         # list
@@ -68,54 +76,67 @@ class courseworkOne:
         #iterating over list A to check if the value set appears in order within list
         if any([val1, val2, val3] == A[i:i + 3] for i in range(len(A) - 1)):
             print("\nYes")
-            print("List containing the given set of elements consecutively and in the given order")
+            print("List containing the given set of elements consecutively and in the given order\n")
         else:
             print("\nNo")
-            print("List not containing the given set of elements consecutively and in the given order")
+            print("List not containing the given set of elements consecutively and in the given order\n")
 
+    #2.1 Counting left and right brackets
     def countingBrackets(self):
+        print("-----------------")
+        print("Exercise 2 - String Processing")
         print("2.1 Counting left and right brackets\n")
-        bracketCounting = str(input("Enter string containing brackets: "))
+        s = str(input("Enter string containing brackets: "))
 
         #counters for left and right brackets
         leftBrackets = 0
         rightBrackets = 0
 
         #checking each element in string and incrementing counters accourding to element
-        for i in bracketCounting:
+        for i in s:
             if i == ')':
                 leftBrackets += 1
             elif i == '(':
                 rightBrackets += 1
-            else:
-                print("No brackets entered")
 
-        print("\nThe string you entered contains %d right brackets and %d left brackets" % (rightBrackets, leftBrackets))
+        print("\nThe string you entered contains %d right brackets and %d left brackets\n" % (rightBrackets, leftBrackets))
 
+    #2.2 Testing whether the string is math-like
     def stringMathLike(self):
+        print("-----------------")
         print("2.2 Testing whether the string is math-like\n")
-        bracketCount = str(input("Enter string containing brackets: "))
-        #dictionary mapping left bracket with right bracket
+        s = str(input("Enter string containing brackets: "))
+        #dictionary holding left and right bracket
         bracketMap = {'(': ')'}
         #stack to add correct math-like brackets
-        stack = []
+        bracketList = []
         mathLike = ""
+        yesOrNo = ""
 
-        for brackets in bracketCount:
-            if brackets in bracketMap:
-                stack.append((bracketMap[brackets]))
-                mathLike = "is math-like"
-            elif not stack or brackets != stack.pop():
+        for brackets in s:
+            #Checking equality in length of string. Making sure string length is even
+            if len(s) % 2 == 0:
+                # add math like bracket which corresponds to dictionary key to list
+                if brackets in bracketMap:
+                    bracketList.append((bracketMap[brackets]))
+                    mathLike = "is math-like"
+                    yesOrNo = "Yes"
+                # checking if last item in bracketlist is not equal to value in bracketList or brackets
+                elif not bracketList or brackets != bracketList.pop():
+                    mathLike = "is not math-like"
+                    yesOrNo = "No"
+                    break
+            else:
                 mathLike = "is not math-like"
+                yesOrNo = "No"
                 break
-
-        print("\nThe string %s %s" % (bracketCount, mathLike))
+        print("\n%s\nThe string %s %s" % (yesOrNo, s, mathLike))
 
     def main(self):
-        #self.checkVals()
-        #self.checkValsConsecutiveOrder()
-        #self.checkValsOrder()
-        #self.countingBrackets()
+        self.checkVals()
+        self.checkValsConsecutiveOrder()
+        self.checkValsOrder()
+        self.countingBrackets()
         self.stringMathLike()
 
 if __name__ == "__main__":
